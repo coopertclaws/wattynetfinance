432585890caf5a6012fc2eed29f809f72536a7fc
Ben Watson <benwatson@bens-mac-mini.lan>
2020-02-09 17:34:06 +0000

    updated db to use coonection pooling

--- db.js
+++ db.js
@@ -1,17 +1,45 @@
 const mysql = require("mysql");
 
 // Create a connection to the database
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DATABASE
+// });
+
+// Create a pool connection to the database
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE
 });
 
-// open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the damn database.");
-});
+// Open the connection
+pool.getConnection((err, connection) => {
+  if (err) {
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+          console.error('Database connection was closed.')
+      }
+      if (err.code === 'ER_CON_COUNT_ERROR') {
+          console.error('Database has too many connections.')
+      }
+      if (err.code === 'ECONNREFUSED') {
+          console.error('Database connection was refused.')
+      }
+  }
+  if (connection) connection.release()
+  return
+})
 
-module.exports = connection;
+module.exports = pool;
+
+// // open the MySQL connection
+// connection.connect(error => {
+//   if (error) throw error;
+//   console.log("Successfully connected to the damn database.");
+// });
+
+// module.exports = connection;
