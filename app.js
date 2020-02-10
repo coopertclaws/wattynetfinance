50df499171a9424f544c7fd36aa323f08c04df64
Ben Watson <benwatson@bens-mac-mini.lan>
2020-02-10 22:52:44 +0000

    API auth not working yet

--- app.js
+++ app.js
@@ -8,10 +8,43 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+
+const oktaJwtVerifier = new OktaJwtVerifier({
+  issuer: process.env.OKTAJWTURL,
+  clientId: process.env.CLIENT_ID,
+  assertClaims: {
+    aud: 'api://default',
+  }
+});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+function authenticationRequired(req, res, next) {
+  const authHeader = req.headers.authorization || '';
+  const match = authHeader.match(/Bearer (.+)/);
+
+  if (!match) {
+    res.status(401);
+    return next('Not Today!');
+  }
+
+  const accessToken = match[1];
+
+  return oktaJwtVerifier.verifyAccessToken(accessToken)
+    .then((jwt) => {
+      req.jwt = jwt;
+      next();
+    })
+    .catch((err) => {
+      res.status(401).send(err.message);
+    });
+}
+
+// app.all('*', authenticationRequired);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +58,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('/api/messages', authenticationRequired, (req, res) => {
+
+  // if (err) {
+  //   console.log('Error', err);
+  // } else {
+  //   console.log('No error');
+  // }
+
+  res.json({
+    messages: [
+      {
+        date:  new Date(),
+        text: 'I am a robot.'
+      },
+      {
+        date:  new Date(new Date().getTime() - 1000 * 60 * 60),
+        text: 'Hello, world!'
+      }
+    ]
+  });
+});
+
+// app.use(function (err, req, res, next) {
+//   if (err) {
+//     console.log('Error', err);
+//   } else {
+//     console.log('404')
+//   }
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,6 +105,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  console.log(err);
 });
 
 module.exports = app;
